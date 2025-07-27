ef61ef1d8bdda98520ff6b387f497f71730f4697
Pharvesh Salman Choudhary <46295733+pharvesh@users.noreply.github.com>
2025-07-27 14:33:01 +0530

    Create early_stopping.py

---
+++ utils/early_stopping.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+
+class EarlyStopping:
+    def __init__(self, patience=40, verbose=False, delta=0.0, path='generator_best.pt', trace_func=print):
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f"EarlyStopping counter: {self.counter}/{self.patience}")
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        if self.verbose:
+            self.trace_func(f"Validation loss decreased ({self.val_loss_min:.6f} → {val_loss:.6f}). Saving model to {self.path}")
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
